6942575e421699d752a609b92d5c59fc6d545e77
JosueCR170 <josolorzano4@gmail.com>
2025-03-01 22:21:20 -0600

    implementacion a docker

--- db/actividades_table.sql
+++ db/actividades_table.sql
@@ -2,46 +2,43 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "America/Costa_Rica";
 
---
--- Verificar si la base de datos eventosdb existe
---
 CREATE DATABASE IF NOT EXISTS eventosdb;
 USE eventosdb;
--- --------------------------------------------------------
---
--- Estructura de tabla para la tabla actividad
---
+
+CREATE TABLE IF NOT EXISTS cliente (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  nombre varchar(50) NOT NULL,
+  apellido1 varchar(40) NOT NULL,
+  apellido2 varchar(40) DEFAULT NULL,
+  correo varchar(80) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY correo (correo)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
 CREATE TABLE IF NOT EXISTS actividad (
-  id int(11) NOT NULL,
+  id int(11) NOT NULL AUTO_INCREMENT,
   nombre varchar(50) NOT NULL,
   fecha date NOT NULL,
   descripcion text DEFAULT NULL,
   cliente int(11) NOT NULL,
-  FOREIGN KEY (cliente) REFERENCES cliente(id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla cliente
---
-CREATE TABLE IF NOT EXISTS cliente (
-  id int(11) NOT NULL,
-  nombre varchar(50) NOT NULL,
-  apellido1 varchar(40) NOT NULL,
-  apellido2 varchar(40) DEFAULT NULL,
-  correo varchar(80) NOT NULL
+  FOREIGN KEY (cliente) REFERENCES cliente(id),
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
-ALTER TABLE actividad
-  ADD PRIMARY KEY (id);
+INSERT INTO cliente (nombre, apellido1, apellido2, correo)
+VALUES
+  ('Juan', 'Pérez', 'Gómez', 'juan.perez@example.com'),
+  ('Ana', 'Martínez', 'López', 'ana.martinez@example.com'),
+  ('Carlos', 'Gómez', 'Sánchez', 'carlos.gomez@example.com'),
+  ('Lucía', 'Rodríguez', NULL, 'lucia.rodriguez@example.com'),
+  ('Marta', 'Hernández', 'Pérez', 'marta.hernandez@example.com');
 
-ALTER TABLE cliente
-  ADD PRIMARY KEY (id),
-  ADD UNIQUE KEY correo (correo);
+INSERT INTO actividad (nombre, fecha, descripcion, cliente)
+VALUES
+  ('Concierto de Rock', '2025-03-15', 'Un concierto de rock en vivo.', 1), 
+  ('Conferencia de Tecnología', '2025-04-01', 'Conferencia sobre el futuro de la tecnología.', 2), 
+  ('Maratón Solidario', '2025-05-20', 'Un maratón para recaudar fondos para la caridad.', 1),
+  ('Exposición de Arte', '2025-06-10', 'Exposición de artistas locales.', 4),  
+  ('Feria Gastronómica', '2025-07-25', 'Feria de comida típica regional.', 2);  
 
-ALTER TABLE actividad
-  MODIFY id int(11) NOT NULL AUTO_INCREMENT;
-
-ALTER TABLE cliente
-  MODIFY id int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
